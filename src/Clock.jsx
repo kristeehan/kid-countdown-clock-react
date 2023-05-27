cedbcca74084b23ca50c0e693552d760ce88714a
kristeehan <kristeehan@gmail.com>
2023-05-27 17:07:11 -0400

    buttons and more styles

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -20,14 +20,38 @@ function Clock({ timeInMinutes }) {
       </div>
       <div id="clock-controls">
         <div className="btn-toolbar mt-3 mb-3">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setClockState(CLOCK_STATES.PLAYING);
-            }}
-          >
-            PLAY
-          </button>
+          <div className="btn-group">
+            <button
+              className="btn btn-primary"
+              id="start"
+              onClick={(e) => {
+                e.preventDefault();
+                setClockState(CLOCK_STATES.PLAYING);
+              }}
+            >
+              Start
+            </button>
+            <button
+              className="btn btn-primary"
+              id="pause"
+              onClick={(e) => {
+                e.preventDefault();
+                setClockState(CLOCK_STATES.PAUSED);
+              }}
+            >
+              Pause
+            </button>
+            <button
+              className="btn btn-primary"
+              id="reset"
+              onClick={(e) => {
+                e.preventDefault();
+                setClockState(CLOCK_STATES.PAUSED);
+              }}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </div>
