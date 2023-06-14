a158da11b43173cedde65e64a38bb4c7c7f7fe0e
kristeehan <kristeehan@gmail.com>
2023-06-13 21:48:12 -0400

    working settings

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,5 +1,23 @@
-function Settings() {
-  return <h3>Settings</h3>;
+function Settings({ setTimeInMinutes }) {
+  return (
+    <div>
+      <h3>Settings</h3>
+      <ul>
+        <li>
+          Time in minutes:{" "}
+          <input
+            type="text"
+            onChange={(e) => {
+              e.preventDefault();
+              let value = parseInt(e.target.value);
+              console.log(value);
+              setTimeInMinutes(value);
+            }}
+          />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Settings;
--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,24 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import Clock from "./Clock";
 import Settings from "./Settings";
 
 function KidCountDownClock() {
   const headline = `When it's all red, it's time for bed!`;
-  const defaultTime = 1;
+  const defaultTime = 5;
+  const [timeInMinutes, setTimeInMinutes] = useState(defaultTime);
   return (
     <BrowserRouter>
       <h1>
         <Link to="/">{headline}</Link>
       </h1>
       <Routes>
-        <Route path="/" element={<Clock timeInMinutes={defaultTime} />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/" element={<Clock timeInMinutes={timeInMinutes} />} />
+        <Route
+          path="/settings"
+          element={<Settings setTimeInMinutes={setTimeInMinutes} />}
+        />
       </Routes>
       <Link to="/settings">Settings</Link>
     </BrowserRouter>
